56df166ff86722ac5d0e873c0c3017556964102b
roshita02 <roshita_shakya@hotmail.com>
2017-12-18 13:21:17 +0545

    changed put to patch for record update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'records#index'
+  get '/records', to: 'records#index'
+
+  get '/records/new', to: 'records#new', as: :new_record
+  post '/records', to: 'records#create'
+
+  get '/records/:id/', to: 'records#edit', as: :edit_record
+  put '/records/:id', to: 'records#edit'
+  patch '/records/:id/', to: 'records#update'
+
+  delete '/records/:id/delete/', to: 'records#destroy', as: :record
+#  delete '/records/:id/delete', to: 'records#destroy'
+
+  resources :posts
 end
