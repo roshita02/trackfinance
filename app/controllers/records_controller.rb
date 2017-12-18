b745d6486efd25047fb6a1dea240359ef389ba0e
roshita02 <roshita_shakya@hotmail.com>
2017-12-18 13:23:25 +0545

    Simple form gem installed

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,5 +1,38 @@
 class RecordsController < ApplicationController
   def index
+    @records = Record.all
+    @balance = Record.balance
+    @debt = Record.debt
+    @total = Record.total
+  end
 
+  def create
+    @records = Record.create(record_params)
+    redirect_to root_path
+
+  end
+
+  def new
+    @record = Record.new
+  end
+
+  def edit
+    @record = Record.find(params[:id])
+  end
+
+  def update
+    @record = Record.find(params[:id])
+    redirect_to root_path if @record.update(record_params)
+  end
+
+  def destroy
+    @record = Record.find(params[:id])
+    @record.destroy
+    redirect_to root_path
+  end
+
+  private
+  def record_params
+    params.require(:record).permit(:title, :date, :amount)
   end
 end
